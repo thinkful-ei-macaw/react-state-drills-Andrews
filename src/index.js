3e8ebe748b5d39d7fb7d33e4dc7b5ba31afbebdf
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-03-04 14:19:07 -0700

    completed drill 3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
 // import HelloWorld from './state-drills/HelloWorld'
-import Bomb from './state-drills/Bomb';
+// import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
 
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
---
+++ src/state-drills/RouletteGun.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+class RouletteGun extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            chamber: null,
+            spinningTheChamber: false
+        }
+        this.timeout = null;
+    }
+
+    static defaultProps = {
+        bulletInChamber: 8
+    }
+
+    handleClick = e => {
+        this.setState({
+            spinningTheChamber: true
+        });
+
+        this.timeout = setTimeout( () => {
+            let random = Math.ceil(Math.random() * 8);
+            this.setState({
+                chamber: random,
+                spinningTheChamber: false
+            })
+        }, 2000)
+    
+}
+
+    clearTimer() {
+         clearTimeout(this.timeout);
+    }
+
+    componentWillUnmount() {
+        this.clearTimer();
+    }
+
+    render() {
+        let output = "";
+        if(this.state.spinningTheChamber === true) {
+            output = "Spinning the chamber and pulling the trigger";
+        }
+        else if(this.state.chamber === this.props.bulletInChamber) {
+            output = "BANG!!!!"
+        }
+        else {
+            output = "you're safe!!"
+        }
+        return (
+            <div>
+                <p>{output}</p>
+                <button onClick={this.handleClick}>Pull the trigger!</button>
+            </div>
+        )
+    }
+}
+
+export default RouletteGun;
